a890a3d42c609af93dcef31b86bc8d1839231c57
Dai Peizhe <daipeizhe@icloud.com>
2022-07-16 21:43:04 +0800

    这是开发人员A的第二次提交！

--- src/main/java/com/pers/javaweb/Test.java
+++ src/main/java/com/pers/javaweb/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello, java web 1 !!!");
         System.out.println("hello, github I am Developer A !!!");
+        System.out.println("hello, 这是开发人员A的第二次提交！！！");
     }
 
 }
