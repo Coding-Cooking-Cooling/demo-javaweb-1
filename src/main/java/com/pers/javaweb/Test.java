26d68de08f1aab2af6efc498969adf2c3405d717
Dai Peizhe <daipeizhe@icloud.com>
2022-07-16 16:04:16 +0800

    这是开发人员A的第一次提交！

--- src/main/java/com/pers/javaweb/Test.java
+++ src/main/java/com/pers/javaweb/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello, java web 1 !!!");
+        System.out.println("hello, github I am Developer A !!!");
     }
 
 }
